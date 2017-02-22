e0ddc9e4d550940093f99eb694bb669c92bcf096
boostNode <support@boostnode.com>
2017-02-21 22:03:11 -0600

    original

---
+++ Module05_Lesson01/Module05_Lesson01/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+*  Author: Troy Davis
+*  Project: Module05_Lesson01 - Console (examples: arrays, methods, switch statements, recursion, try-catch)
+*  Class: IN 2017 (Advanced C#)
+*  Date: Feb 21, 2017 
+*  Revision: Original
+*/
+
+using System;
+
+namespace Module05_Lesson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // greeting
+            Console.WriteLine("Hello from the Module 05 Main Method - Lesson 01");
+
+            // EXAMPLE 1: arrays (see ... below)
+
+
+            // wait on user to close console
+            Console.Write("\nPress 'Enter' to exit: "); Console.ReadLine();
+        }
+    }
+}
